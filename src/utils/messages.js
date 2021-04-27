492676707a23fe97092ff10f21be5c5d3bf4b403
unknown <yara.yassien@gmail.com>
2021-04-27 02:55:18 +0200

    init commit

---
+++ src/utils/messages.js
@@ -0,0 +1,39 @@
+/*const generateMessage=(text)=>{
+    return {
+        text,
+        createdAt:new Date().getTime()
+    }
+}
+
+const generateLocationMessage=(url)=>{
+   return{
+    url,
+    createdAt:new Date().getTime()
+   }
+}
+module.exports={
+    generateMessage,
+    generateLocationMessage
+}
+*/
+
+const generateMessage = (username ,text) => {
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime()
+    }
+}
+
+const generateLocationMessage = (username,url) => {
+    return {
+        username,
+        url,
+        createdAt: new Date().getTime()
+    }
+}
+
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+}
